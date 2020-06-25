a815044024ebf613ebd459b92db2099fedba947b
agabriel.nieve <agabriel.nieve@gmail.com>
2020-06-25 23:41:18 +0800

    printing update

--- datatable.js
+++ datatable.js
@@ -78,8 +78,20 @@ function writeToList(data) {
 
 function printId() {
 	const btnPrint = document.getElementById('btn-print')
+	const framePrint = document.getElementById('iframe-id')
+
 	btnPrint.addEventListener('click', function() {
-		console.log('nice')
+		var doc = new jsPDF();
+		doc.text(20, 20, 'Hello world!')
+		doc.text(20, 30, 'This is client-side Javascript, pumping out a PDF.')
+		doc.addPage()
+		doc.text(20, 20, 'Do you like that?')
+		
+		const blob = doc.output('blob')
+		const blobURL = URL.createObjectURL(blob)
+		console.log(framePrint)
+		framePrint.src = blobURL
+		// doc.save('Test.pdf');
 	})
 }
 
